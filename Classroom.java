8bfd8716cd3864b31de75c842b561f459b55c98e
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-04 14:38:20 +0100

    first part

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+    public static void main(String[] args) {
+        Wilder Chloe = new Wilder("Chloe",true);
+        Wilder Yahia= new Wilder("Yahia",true);
+        Wilder Charles = new Wilder("Charles",false);
+
+
+        System.out.println(Chloe.whoAmI());
+        System.out.println(Charles.whoAmI());
+    }
+}
--- Wilder.java
+++ Wilder.java
@@ -1,3 +1,42 @@
 public class Wilder{
-    
+
+    //Add attributs
+    private String firstname;
+    private boolean aware;
+
+    // constructors
+    public Wilder(String firstname, boolean aware){
+        this.firstname=firstname;
+        this.aware=aware;
+    }
+
+    //getter
+    public String getFirstName(){
+        return this.firstname;
+    }    
+
+    public boolean isAware(){
+        return this.aware;
+    }
+
+    //setter
+    public void setFirstName(String firstname){
+        this.firstname=firstname;
+    }
+
+    public void setAware(boolean aware){
+        this.aware=aware;
+    }
+
+    //methods
+    public String whoAmI(){
+        String reponse="";
+        reponse = "Je m'appelle " + this.getFirstName();
+        if(this.aware){
+            reponse = reponse + " et je suis aware";
+        } else {
+            reponse = reponse + " et je ne suis pas aware";
+        }
+        return reponse;
+    }
 }
